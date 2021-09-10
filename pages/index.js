f62da84ba81cc36ec638c15dfe639ab88f032f82
Adrian Marin <adrian@adrianthedev.com>
2021-09-10 15:11:21 +0300

    add error cases

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useRouter } from "next/router"
 
 export default function Home() {
+  const router = useRouter()
+  if (router.query.error) throw new Error("Client error.");
+
   return (
     <div className={styles.container}>
       <Head>
